8c18a542a1bf85d892a7bac9b9925fdd21ec6539
cckinwest <kensechan@gmail.com>
2023-05-21 20:24:24 +0100

    chikin works on hasan's change and create buttons for history

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,51 @@ var chartContainer = document.getElementById("myChart");
 var newsPanel = document.getElementById("newsPanel");
 var historyPanel = document.getElementById("historyPanel");
 
+var stockSuccess = true;
+var newsSuccess = true;
+
+function CreateChart(labels, close, open) {
+  var stockChart = document.createElement("canvas");
+
+  new Chart(stockChart, {
+    type: "line",
+    data: {
+      labels: labels,
+
+      datasets: [
+        {
+          label: "Close",
+          data: close,
+          borderWidth: 2,
+          borderColor: "#4C5CA2",
+        },
+        {
+          label: "Open",
+          data: open,
+          borderWidth: 2,
+          borderColor: "#8A6FC4",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+
+  chartContainer.appendChild(stockChart);
+}
+
 async function DisplayStockData(stocksTicker, from, to) {
   while (chartContainer.firstChild) {
     chartContainer.removeChild(chartContainer.firstChild);
   }
 
-  var stockChart = document.createElement("canvas");
-
   const APIKEY = "79G4QU6AaADL93J2chBjRQKru3lIvD8z";
   const Aggregates = "https://api.polygon.io/v2/aggs/ticker";
-  //const multiplier = 1;
-  //const timespan = "day";
 
   var url = `${Aggregates}/${stocksTicker}/range/1/day/${from}/${to}?adjusted=true&sort=asc&limit=120&apiKey=${APIKEY}`;
 
@@ -27,38 +61,18 @@ async function DisplayStockData(stocksTicker, from, to) {
     const open = data.results.map((entry) => entry.o);
     const close = data.results.map((entry) => entry.c);
 
-    new Chart(stockChart, {
-      type: "line",
-      data: {
-        labels: labels,
+    var chartTitle = document.createElement("h2");
+    chartTitle.textContent = `${stocksTicker} (${from} to ${to})`;
 
-        datasets: [
-          {
-            label: "Close",
-            data: close,
-            borderWidth: 2,
-            borderColor: "#4C5CA2",
-          },
-          {
-            label: "Open",
-            data: open,
-            borderWidth: 2,
-            borderColor: "#8A6FC4",
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: false,
-          },
-        },
-      },
-    });
+    chartContainer.appendChild(chartTitle);
+    CreateChart(labels, close, open);
 
-    chartContainer.appendChild(stockChart);
+    stockSuccess = true;
   } catch (error) {
-    console.log(`Error fetching stock : ${error}`);
+    var errorMsg = document.createElement("h1");
+    errorMsg.textContent = `Error fetching stock : ${error}`;
+    chartContainer.appendChild(errorMsg);
+    stockSuccess = false;
   }
 }
 
@@ -79,119 +93,131 @@ async function DisplayNews(stocksTicker, from, to) {
     newsPanel.removeChild(newsPanel.firstChild);
   }
 
-  const newsData = await getNews(stocksTicker, from, to);
+  try {
+    const newsData = await getNews(stocksTicker, from, to);
 
-  for (var i = 0; i < newsData.length; i++) {
-    var newsBox = document.createElement("div");
-    newsBox.setAttribute("class", "news-box");
+    for (var i = 0; i < newsData.length; i++) {
+      var newsBox = document.createElement("div");
+      newsBox.setAttribute("class", "news-box");
 
-    var newsDate = document.createElement("h3");
-    newsDate.textContent = `${newsData[i].publishedAt.slice(0, 10)}`;
+      var newsDate = document.createElement("h3");
+      newsDate.textContent = `${newsData[i].publishedAt.slice(0, 10)}`;
 
-    var newsTitle = document.createElement("p");
-    newsTitle.textContent = `${newsData[i].title}`;
+      var newsTitle = document.createElement("p");
+      newsTitle.textContent = `${newsData[i].title}`;
 
-    var newsURL = document.createElement("a");
-    newsURL.textContent = "Link to the news";
-    newsURL.setAttribute("href", newsData[i].url);
+      var newsURL = document.createElement("a");
+      newsURL.textContent = "Link to the news";
+      newsURL.setAttribute("href", newsData[i].url);
 
-    newsBox.appendChild(newsDate);
-    newsBox.appendChild(newsTitle);
-    newsBox.appendChild(newsURL);
+      newsBox.appendChild(newsDate);
+      newsBox.appendChild(newsTitle);
+      newsBox.appendChild(newsURL);
 
-    newsBox.setAttribute("style", "border-radius: 10px");
+      newsBox.setAttribute("style", "border-radius: 10px");
 
-    newsPanel.appendChild(newsBox);
-    newsPanel.setAttribute("style", "height: 80vh; overflow: auto;");
+      newsPanel.appendChild(newsBox);
+      newsPanel.setAttribute("style", "height: 80vh; overflow: auto;");
+    }
+    newsSuccess = true;
+  } catch (error) {
+    var errorMsg = document.createElement("h1");
+    errorMsg.textContent = `Error fetching news : ${error}`;
+    chartContainer.appendChild(errorMsg);
+    newsSuccess = false;
   }
 }
 
-function DisplayHistory(stockNameArr, startDateArr, endDateArr) {
+function updateTickerHistory(tickerHistory) {
   while (historyPanel.firstChild) {
     historyPanel.removeChild(historyPanel.firstChild);
   }
 
-  for (var i = 0; i < stockNameArr.length; i++) {
-    var historyBtn = document.createElement("button");
-    historyBtn.setAttribute("class", "news-box");
+  for (var i = tickerHistory.length - 1; i >= 0; i--) {
+    var historyBox = document.createElement("button");
+    historyBox.setAttribute("class", "history-box");
 
     var stockName = document.createElement("h3");
-    stockName.textContent = stockNameArr[i];
+    stockName.textContent = tickerHistory[i];
 
-    var period = document.createElement("p");
-    period.textContent = `from ${startDateArr[i]} to ${endDateArr[i]}`;
+    historyBox.appendChild(stockName);
 
-    historyBtn.appendChild(stockName);
-    historyBtn.appendChild(period);
+    historyBox.setAttribute("data-stock", tickerHistory[i]);
 
-    historyBtn.setAttribute("style", "border-radius: 10px; width: 95%");
+    historyBox.setAttribute("style", "border-radius: 10px; width: 95%");
 
-    historyBtn.addEventListener("click", function () {
-      DisplayStockData(stockNameArr[i], startDateArr[i], endDateArr[i]);
-      DisplayNews(stockNameArr[i], startDateArr[i], endDateArr[i]);
+    historyBox.addEventListener("click", function () {
+      const stockName = this.dataset.stock;
+
+      const startDate = dayjs().subtract(7, "day").format("YYYY-MM-DD");
+      const endDate = dayjs().format("YYYY-MM-DD");
+
+      DisplayStockData(stockName, startDate, endDate);
+      DisplayNews(stockName, startDate, endDate);
     });
 
-    historyPanel.appendChild(historyBtn);
+    historyPanel.appendChild(historyBox);
     historyPanel.setAttribute("style", "height: 80vh; overflow: auto;");
   }
 }
 
 clearBtn.addEventListener("click", function () {
-  localStorage.removeItem("stockName");
-  localStorage.removeItem("startDate");
-  localStorage.removeItem("endDate");
+  localStorage.removeItem("tickerHistory");
+  updateTickerHistory([]);
 });
 
 searchBtn.addEventListener("click", async function () {
-  //chikin's code, make localStorage stores array of search history.
-  var stockNameArr = [];
-  if (localStorage.getItem("stockName")) {
-    stockNameArr = JSON.parse(localStorage.getItem("stockName"));
+  stockSuccess = true;
+  newsSuccess = true;
+
+  var tickerHistory = [];
+  if (localStorage.getItem("tickerHistory")) {
+    tickerHistory = JSON.parse(localStorage.getItem("tickerHistory"));
   }
 
-  var startDateArr = [];
-  if (localStorage.getItem("startDate")) {
-    startDateArr = JSON.parse(localStorage.getItem("startDate"));
-  }
-
-  var endDateArr = [];
-  if (localStorage.getItem("endDate")) {
-    endDateArr = JSON.parse(localStorage.getItem("endDate"));
-  }
-
-  //hasan's code
   var stockName = document.getElementById("stock-input").value;
   var startDate = document.getElementById("start-date-input").value;
+  tickerHistory;
   var endDate = document.getElementById("end-date-input").value;
 
-  stockNameArr.push(stockName);
-  startDateArr.push(startDate);
-  endDateArr.push(endDate);
+  await DisplayStockData(stockName, startDate, endDate);
+  await DisplayNews(stockName, startDate, endDate);
 
-  localStorage.setItem("stockName", JSON.stringify(stockNameArr));
-  localStorage.setItem("startDate", JSON.stringify(startDateArr));
-  localStorage.setItem("endDate", JSON.stringify(endDateArr));
-
-  DisplayHistory(stockNameArr, startDateArr, endDateArr);
-  DisplayStockData(stockName, startDate, endDate);
-  DisplayNews(stockName, startDate, endDate);
+  if (stockSuccess && newsSuccess && !tickerHistory.includes(stockName)) {
+    tickerHistory.push(stockName);
+    localStorage.setItem("tickerHistory", JSON.stringify(tickerHistory));
+    updateTickerHistory(tickerHistory);
+  }
 });
 
-window.addEventListener("load", function () {
-  var stockNameArr = [];
-  if (localStorage.getItem("stockName")) {
-    stockNameArr = JSON.parse(localStorage.getItem("stockName"));
+window.addEventListener("DOMContentLoaded", function () {
+  stockSuccess = true;
+  newsSuccess = true;
+
+  var tickerHistory = [];
+  if (localStorage.getItem("tickerHistory")) {
+    tickerHistory = JSON.parse(localStorage.getItem("tickerHistory"));
   }
 
-  var startDateArr = [];
-  if (localStorage.getItem("startDate")) {
-    startDateArr = JSON.parse(localStorage.getItem("startDate"));
-  }
+  if (tickerHistory.length > 0) {
+    const stockName = tickerHistory[tickerHistory.length - 1];
+    const startDate = dayjs().subtract(7, "day").format("YYYY-MM-DD");
+    const endDate = dayjs().format("YYYY-MM-DD");
 
-  var endDateArr = [];
-  if (localStorage.getItem("endDate")) {
-    endDateArr = JSON.parse(localStorage.getItem("endDate"));
-  }
+    document.getElementById("stock-input").value = stockName;
+    document.getElementById("start-date-input").value = startDate;
+    document.getElementById("end-date-input").value = endDate;
 
-  DisplayHistory(stockNameArr, startDateArr, endDateArr);
+    DisplayStockData(stockName, startDate, endDate);
+
+    DisplayNews(stockName, startDate, endDate);
+
+    updateTickerHistory(tickerHistory);
+  } else {
+    const startDate = dayjs().subtract(7, "day").format("YYYY-MM-DD");
+    const endDate = dayjs().format("YYYY-MM-DD");
+
+    DisplayStockData("AAPL", startDate, endDate);
+    DisplayNews("AAPL", startDate, endDate);
+  }
 });
